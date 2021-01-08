b5f3d834611e83ed53318977a2c4610522c5039c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-08 13:56:30 +0000

    Deploy HicksonWei/homepage_v2 to github.com/HicksonWei/homepage_v2.git:gh-pages

---
+++ worksData.js
@@ -0,0 +1,40 @@
+export const worksList = {
+  data: [
+    {
+      link: 'https://hicksonwei.github.io/HexSchool_RWD_FinalProject/',
+      img: 'works1.jpg',
+      title: '六角西餐廳',
+      content: '客製 RWD + SCSS + jQuery'
+    },
+    {
+      link: 'https://web-dev-memo.netlify.app/',
+      img: 'works2.png',
+      title: '個人 Blog',
+      content: '使用 Hugo 建置'
+    },
+    {
+      link: 'https://hicksonwei.github.io/Landing_Page/',
+      img: 'works3.jpg',
+      title: '六角 Landing Page',
+      content: 'Bootstrap 4 + gulp 管理'
+    },
+    {
+      link: 'https://hicksonwei.github.io/meme-generator/',
+      img: 'works4.png',
+      title: 'Meme generator',
+      content: 'Vanilla JS + gulp 管理'
+    },
+    {
+      link: 'https://hicksonwei.github.io/todos/',
+      img: 'works5.jpg',
+      title: 'todos',
+      content: '簡易 todoList (LocalStorage)'
+    },
+    {
+      link: 'https://audio-searcher.now.sh/',
+      img: 'works6.jpg',
+      title: '單字發音 audio 程式碼擷取工具',
+      content: 'Nuxt + Express API'
+    }
+  ]
+}
